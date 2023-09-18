208c5194e6a942ba00165777646398c61015488a
urzp <ermak80_pass@mail.ru>
2023-09-18 21:00:56 +0300

    config webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
 module.exports = {
-    entry: path.resolve(__dirname, 'scr/main.js')
-}
+    entry: path.resolve(__dirname, 'scr/main.js'),
+
+    //выход
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+
+    // работа с модулями
+    modules:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, //исключаем js файлы 
+                use: 'babel-loader', // прогоняем все js файлы через babel-loader
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: 'vue-loader',
+            },
+        ]
+    },
+
+    // работа с плагинами
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'scr/index.html'),
+        }),
+        new VueLoaderPlugin()
+    ]
+
+};
+
+
+
